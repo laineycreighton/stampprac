69672ea02e5fcf5f769140272dcf1ffdfef42d3b
Lainey Creighton <dev.lainey@gmail.com>
2026-07-24 10:52:19 -0700

    Added content and styling to the Lineup page

--- src/pages/Lineup.jsx
+++ src/pages/Lineup.jsx
@@ -4,15 +4,21 @@
 import '../assets/css/Lineup.css'
 
 //Images
-
+import lineup from '../assets/images/festivallineup.jpg'
 
 //----- EXPORTS -----//
 export default function Lineup() {
     return (
-    <div>
-        <main>
-        <p>Lineup</p>
-        </main>
-    </div>
-);
+        <div className="lineup-container">
+
+            {/*----- IMAGE -----*/}
+            <img src={lineup} alt="festival lineup schedule" />
+
+            {/*----- ARTISTS -----*/}
+            {/* <div className="artists-block">
+                <p>ARTISTS</p>
+            </div> */}
+
+        </div>
+    );
 }
